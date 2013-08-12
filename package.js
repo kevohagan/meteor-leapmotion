e64c27847e7b98422abad9830d87106e837ab929
Kevin O'Hagan <kevinohagan@gmail.com>
2013-08-12 14:29:59 +0200

    change release

--- package.js
+++ package.js
@@ -2,10 +2,6 @@ Package.describe({
   summary: "Include Leap.js in your Meteor Project"
 });
 
-Npm.depends({
-    "leapjs" : "0.2.0"
-    );
-
 Package.on_use(function(){
     api.add_files('leap.min.js','client');
 });
