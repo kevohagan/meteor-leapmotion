abaef6cb4180da4eab21ff520bce5c0fb6bc8fa7
Kevin O'Hagan <kevinohagan@gmail.com>
2013-08-12 14:40:18 +0200

    leapmanager

--- package.js
+++ package.js
@@ -1,7 +1,9 @@
 Package.describe({
-  summary: "Include Leap.js in your Meteor Project"
+  summary: "LeapMotion interaction in your Meteor Project"
 });
 
 Package.on_use(function(){
-    api.add_files('leap.min.js','client');
+    api.add_files('/js/leap.min.js','client');
+    api.add_files('/js/leap-manager.min.js','client');
+    api.add_files('/css/leap-manager.css','client');
 });
